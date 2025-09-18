ac70f8f74814daa1dba2a51a7ba40ce087f118fb
Emanuel González Gartland <gonzlezgartlandemanuel@gmail.com>
2025-09-17 21:01:02 -0300

    Carrera de caballos

--- animal.py
+++ animal.py
@@ -1,45 +1,40 @@
-from abc import ABC, abstractmethod
+import random
+import time
+import os
 
-class Animal(ABC):
-    @abstractmethod
-    def speak(self):
-        pass
+class Caballo(object):
+    def __init__(self, name, f, t):
+        self.name = name
+        self.posicion = 0
+        self.f = f
+        self.t = t
+    
+    def corre(self):
+        p = random.randint(self.f, self.t)
+        self.posicion = self.posicion + p
 
-    @abstractmethod
-    def eat(self):
-        pass
+    def dibuja(self):
+        x = ("-" * self.posicion) + f"[{self.name}]"
+        print(x)
 
-    @abstractmethod
-    def sleep(self):
-        pass
+pista = [
+    Caballo("Yatasto", 1, 7), 
+    Caballo("Pingo", 1, 6), 
+    Caballo("Reina", 1, 5), 
+    Caballo("Negrito", 1, 5), 
+    Caballo("CocaCola", 1, 5), 
+    Caballo("Alfajor", 1, 5)
+]
 
-    @abstractmethod
-    def move(self):
-        pass
+termino = False
+while termino == False:
+    i = 0
+    os.system("clear")
+    while i < len(pista):
+        pista[i].corre()
+        pista[i].dibuja()
+        if pista[i].posicion > 100:
+            termino = True
+        i = i + 1
 
-
-class Perro(Animal):
-    def speak(self):
-        print("El perro ladra")
-
-    def eat(self):
-        print("El perro come")
-
-    def sleep(self):
-        print("El perro duerme")
-
-    def move(self):
-        print("El perro camina")
-
-class Gato(Animal):
-    def speak(self):
-        print("El gato maulla")
-
-    def eat(self):
-        print("El gato come")
-
-    def sleep(self):
-        print("El gato duerme")
-
-    def move(self):
-        print("El gato camina")
+    time.sleep(0.5)
